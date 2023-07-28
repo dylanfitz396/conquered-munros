e27c614879584e13495d0c82a96a39fef8a1be4e
Dylan Fitzgerald <dylanfitz396@gmail.com>
2023-07-28 22:37:20 +0100

    feat: create dropdown component

--- app/filter/page.tsx
+++ app/filter/page.tsx
@@ -1,10 +1,14 @@
+'use client'
+
+import Dropdown from "@/components/Dropdown";
 import Navbar from "../../components/Navbar";
 
-export default async function Filter() {
+export default function Filter() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <Navbar/>
-      <h2 className="text-xl">Work In Progress</h2>
+      <h2 className="text-xl">Choose from the below filters</h2>
+      <Dropdown dropdownHeading="Food attribute" dropdownItems={['family', 'order', 'genus']} />
     </main>
   )
 }
